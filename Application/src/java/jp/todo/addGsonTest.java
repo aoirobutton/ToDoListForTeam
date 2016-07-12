7647575f20995fa53d0d88e15862102842a7cf10
kyoumoto <q677224k@mail.kyutech.jp>
2016-07-13 00:28:30 +0900

    Built in gson

---
+++ Application/src/java/jp/todo/addGsonTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.todo;
+
+import com.google.gson.Gson;
+/**
+ *
+ * @author matsushita
+ */
+public class addGsonTest {
+    public addGsonTest(){
+    }
+    
+    public Bean func(String form){
+        Gson gson = new Gson();
+        Bean ret = gson.fromJson(form, Bean.class);
+        return ret;//gson.toJson(ret, Bean.class);
+    }
+}
