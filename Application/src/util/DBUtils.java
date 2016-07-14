cec14ebce2a46e6a102de891803b521ecb3127e8
aoiro_makoto <aoiro_makoto@klab.ai.kyutech.ac.jp>
2016-07-14 13:59:37 +0900

    ちょっと修正

--- Application/src/util/DBUtils.java
+++ Application/src/util/DBUtils.java
@@ -5,11 +5,11 @@
  */
 package util;
 
-import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import com.mongodb.DBObject;
 import com.mongodb.Mongo;
-import com.mongodb.MongoException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -20,7 +20,7 @@ public class DBUtils {
     
         private static Mongo m;
     private static DB db;
-    private static final String dbName = "tem";
+    private static final String dbName = "Todo";
 
     private static DBUtils singleton = new DBUtils();
 
@@ -34,9 +34,52 @@ public class DBUtils {
 
     // invisible singleton constructor
     private DBUtils() {
-            m = new Mongo("localhost", 27017);
-            db = m.getDB(dbName);
+            try {
+                m = new Mongo("localhost", 27017);
+                db = m.getDB(dbName);
+            } catch (UnknownHostException ex) {
+                Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
 
+    public DB getDb() {
+        return db;
+    }
+
+        /**
+     * <p>
+     * HTML特殊文字のサニタイズを行う．
+     * ユーザが入力した文字列のうち，セキュリティ上危険な特殊タグ文字を全て置換する．
+     * 基本的に String に対する setter の全てで呼び出すこと．<br>
+     * ※ユーザが入力した & や <> などの特殊文字をそのまま利用すると，
+     * DBへの命令を直接埋め込まれる危険性がある（XSS，SQLインジェクション）．
+     * </p><p>
+     * 利用例：
+     * </p>
+     * <pre>
+     *   public void setMessage(String message) {
+     *     this.message = DBUtils.getInstance().sanitize(message);
+     *
+     *     // 直接代入しないこと
+     *     // this.message = message;
+     *   }
+     * </pre>
+     *
+     * @param str サニタイズ対象の文字列
+     * @return サニタイズ済み文字列
+     */
+    public static String sanitize(String str) {
+        if (str == null) {
+            return "";
+        }
+        str = str.replaceAll("&" , "&amp;" );
+        str = str.replaceAll("<" , "&lt;"  );
+        str = str.replaceAll(">" , "&gt;"  );
+        str = str.replaceAll("\"", "&quot;");
+        str = str.replaceAll("'" , "&#39;" );
+        return str;
+    }
+
+    
     
 }
