85e3f318d30c32616e64fcbcd6985b82bd83479f
kyoumoto <q677224k@mail.kyutech.jp>
2016-07-15 17:34:03 +0900

    LoginUserUtils.java を作った.

---
+++ Application/src/java/util/LoginUserUtils.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import com.google.gson.Gson;
+import model.Account;
+
+/**
+ *
+ * @author matsushita
+ */
+public class LoginUserUtils{
+    private static Account singleton = new Account();
+    
+    private LoginUserUtils(String form){
+        Gson gson = new Gson();
+        singleton = gson.fromJson(form, Account.class);
+    }
+    
+    public static int getUserId(){
+        return singleton.getId();
+    }
+    
+}
