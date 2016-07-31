ecde3dab86fa633052c963aebb2c3ea5f91d53fb
yashima <nassi3312panda@yahoo.co.jp>
2016-07-31 10:43:49 +0900

    チケットの追加(ToDoの追加機能)の実装完了 動作確認済み

---
+++ Application/src/java/controller/AddTicketController.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import java.util.ArrayList;
+import java.util.List;
+import model.Project;
+import util.TicketCollectionUtils;
+import model.Ticket;
+import util.AccountCollectionUtils;
+import util.ProjectCollectionUtils;
+import util.LoginUserUtils;
+
+import com.mongodb.DB;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import model.Account;
+import util.DBUtils;
+/**
+ *
+ * @author matsushita
+ */
+public class AddTicketController {
+    
+    private Gson gson = new Gson();
+    
+    public AddTicketController(){
+    }
+    
+    public boolean AddTicket(Ticket ticket){
+        // チケットのコレクションを取って来る
+        DBCollection ticketCollection = TicketCollectionUtils.getInstance().getCollection();
+        
+        // 被り防止用の検索クエリー作成
+        // プロジェクト内に同じチケット名がないか確認
+        BasicDBObject doc = new BasicDBObject("ticket",ticket.getTicket())
+                .append("project", ticket.getProject());
+        // 検索
+        DBCursor coll = ticketCollection.find(doc);
+        if(coll.size() == 1){
+            // 既に同じ名前のチケットがある
+            // 何もせずに終了
+            return false;
+        }
+        
+        // ここまで来たら被ってないので登録
+        // チケット登録用のクエリーを作成
+        BasicDBObject query = new BasicDBObject("ticket",ticket.getTicket())
+                .append("responsible", ticket.getResponsible())
+                .append("state", ticket.getState())
+                .append("description", ticket.getDescription())
+                .append("deadline", ticket.getDeadline())
+                .append("project", ticket.getProject());
+        // チケット登録して終了
+        ticketCollection.insert(query);
+        
+        return true;
+    }
+    
+    //////////////////////////////////////////// まだ途中
+    public List<Account> getAccountList(){
+        // ユーザー一覧を格納するリスト
+        List<Account> accountList = new ArrayList<Account>();
+        // 現在ログインしているユーザ名
+        String user = LoginUserUtils.getInstance().getLoginUser().getUser();
+        
+        // アカウントのコレクションを取って来る
+        DBCollection accountCollection = AccountCollectionUtils.getInstance().getCollection();
+        
+        // 自分が所属しているプロジェクトの一覧を取って来る
+        List<Project> projectList = new ArrayList<Project>();
+        projectList = getProjectList();
+        
+        Account ac = new Account();
+        
+        // 重複防止用のユーザ名リスト
+        List<String> overlapList = new ArrayList<String>();
+        // 一回でも検索にヒットしたかのフラグ
+        boolean hit = false;
+        for(Project p : projectList) {
+            // それぞれのプロジェクト用にユーザ検索用のクエリーを作成
+            BasicDBObject query = new BasicDBObject("project", p.getProject());
+            // 検索
+            DBCursor cursor = accountCollection.find(query);
+            if(cursor.size() >= 1){
+                while(cursor.hasNext()){
+                    ac = gson.fromJson(cursor.next().toString(), Account.class);
+                    if(!overlapList.contains(ac.getUser())) {
+                        accountList.add(ac);
+                        overlapList.add(ac.getUser());
+                        hit = true;
+                    }
+                }
+            }
+        }
+        
+        if(hit) {
+            return accountList;
+        } else {
+            return null;
+        }
+    }
+    
+    public List<Project> getProjectList(){
+        // プロジェクト一覧を格納するリスト
+        List<Project> projectList = new ArrayList<Project>();
+        // 現在ログインしているユーザ名
+        String user = LoginUserUtils.getInstance().getLoginUser().getUser();
+        
+        // プロジェクトのコレクションを取って来る
+        DBCollection projectCollection = ProjectCollectionUtils.getInstance().getCollection();
+        
+        // ログインユーザが所属しているプロジェクトを検索
+        // プロジェクト登録用のクエリーを作成
+        BasicDBObject query = new BasicDBObject("member", user);
+        // 検索
+        DBCursor cursor = projectCollection.find(query);
+        if(cursor.size() >= 1){
+            // ユーザが所属しているプロジェクトが１つ以上ある
+            Project p = new Project();
+            while(cursor.hasNext()){
+                p = gson.fromJson(cursor.next().toString(), Project.class);
+                projectList.add(p);
+            }
+            return projectList;
+        } else {
+            return null;
+        }
+    }
+}
