820813d149c4210fac4653bf01ef42642c7edd39
aoiro_button <aoiro_makoto@MacBook-Air.local>
2016-07-31 20:14:39 +0900

    バグチェック

---
+++ Application/src/java/controller/EditTicketController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import java.util.ArrayList;
+import java.util.List;
+import util.TicketCollectionUtils;
+import util.LoginUserUtils;
+import model.Ticket;
+import util.DBUtils;
+
+
+
+/**
+ *
+ * @author aoiro_makoto
+ */
+public class EditTicketController {
+
+    private DBCollection ticketCollection;// = TicketCollectionUtils.getInstance().getCollection();
+    private DB db = DBUtils.getInstance().getDb();
+    private DBCollection projectCollection = db.getCollection("project");
+    //private DBCollection accountColl = TicketCollectionUtils.getCollection();
+    private Gson gson = new Gson();
+
+    public EditTicketController(){
+        ticketCollection = TicketCollectionUtils.getInstance().getCollection();        
+    }
+
+    public boolean editTicket(Ticket ticket, String responsible, String state, String description, String deadline){
+        DBCollection ticketCollection = TicketCollectionUtils.getInstance().getCollection();
+        BasicDBObject query = new BasicDBObject("ticket",ticket.getTicket())
+                .append("project", ticket.getProject());
+        DBCursor cursor = ticketCollection.find(query);
+        if(cursor.size() == 1){
+            ticketCollection.remove(query);
+            return true;
+        }
+        return false;
+    }
+
+
+}
